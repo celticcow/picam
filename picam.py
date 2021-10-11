3e536ba1225ac68747ec10fad87387e2387c5e4d
celticcow <gregory.dunlap@gmail.com>
2021-10-11 14:49:33 -0500

    basic code

--- picam.py
+++ picam.py
@@ -1,3 +1,14 @@
 #!/usr/bin/python3
 
+from picamera import PiCamera
+from time import sleep
 
+def main():
+    camera = PiCamera()
+
+    camera.start_preview()
+    sleep(5)
+    camera.stop_preview()
+
+if __name__ == "__main__":
+    main()
