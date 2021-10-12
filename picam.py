f2c8ef3a8ae40a920ecad829e4c1aeb1c95db698
celticcow <gregory.dunlap@gmail.com>
2021-10-11 19:26:55 -0500

    playing with options

--- picam.py
+++ picam.py
@@ -6,8 +6,12 @@ from time import sleep
 def main():
     camera = PiCamera()
 
+    camera.resolution = (800, 600)
+    camera.framerate = 15
+
     camera.start_preview()
     sleep(5)
+    camera.capture('/home/pi/Desktop/pic.jpg')
     camera.stop_preview()
 
 if __name__ == "__main__":
